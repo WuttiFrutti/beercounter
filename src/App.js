d69cce467cd3992a15b9a53a8fc3a7f0ea23c42a
Wutti <woutkn@gmail.com>
2022-02-08 17:38:51 +0100

    added darkmode to meta change

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import { light, dark } from "./Config/Theme"
 import { ThemeProvider } from '@mui/system';
 import Snack from './Components/Global/Snack';
 import { MainStore } from './Config/MainStore';
+import { useEffect } from 'react';
 
 function App() {
   const darkmode = MainStore.useState(s => s.darkmode);
+
+  useEffect(() => {
+    document.querySelector("meta[name='theme-color']").setAttribute("content", darkmode ? "#272727" : "#FFFFFF");
+  },[darkmode])
 
   return (
     <ThemeProvider theme={darkmode ? dark : light}>
