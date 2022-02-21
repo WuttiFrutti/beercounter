a7f27ff04fa9dca7d88b06b0e0a0eb06ce3b0d86
Wutti <woutkn@gmail.com>
2022-02-21 23:22:32 +0100

    scroll test

--- src/Pages/Page.jsx
+++ src/Pages/Page.jsx
@@ -7,7 +7,7 @@ import { Box } from '@mui/system';
 
 const Page = (args) => {
 
-    return <Box {...args} sx={{minWidth:"100vw", overflowX:"hidden",...args.sx}}>
+    return <Box {...args} sx={{minWidth:"100vw",...args.sx}}>
         {args.children}
     </Box>
 }
